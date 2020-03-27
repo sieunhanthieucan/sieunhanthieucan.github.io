ede58b3840786277aca25623e8d5bbb7cca7c2f9
sieunhanthieucan <62739528+sieunhanthieucan@users.noreply.github.com>
2020-03-27 14:41:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const button = document.querySelector('button');
 button.addEventListener('click', function(e) {
     fetch(`${location.origin}/more.json`)
         .then(res => res.json())
-        .then(dat => console.log(dat))
+        .then(dat => {
+            console.log(this, window, self, AMP, dat);
+        })
     ;
 });
