47a0cdf573bb464c79444f9cc806f3bf152a9a5a
sieunhanthieucan <62739528+sieunhanthieucan@users.noreply.github.com>
2020-03-27 14:47:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ button.addEventListener('click', function(e) {
     fetch(`${location.origin}/more.json`)
         .then(res => res.json())
         .then(dat => {
-            Object.assign(AMP, { dat });
+            Object.assign(window, { dat });
             console.log(this, window, self, AMP, dat);
         })
     ;
